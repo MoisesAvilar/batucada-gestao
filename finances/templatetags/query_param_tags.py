7d13ee0560ed438663c2cb15c877cf7169159adf
Moisés <moiseavilar0@gmail.com>
2025-10-03 11:11:30 -0300

    feat: Add 'smart_title' filter for text capitalization while keeping acronyms uppercase

--- finances/templatetags/query_param_tags.py
+++ finances/templatetags/query_param_tags.py
@@ -19,3 +19,30 @@ def dict_minus(query_dict, key_to_remove):
         
     # Retorna os parâmetros restantes codificados para URL
     return params.urlencode()
+
+
+@register.filter(name='smart_title')
+def smart_title(value):
+    """
+    Capitaliza o texto como o filtro 'title', mas mantém siglas pré-definidas em maiúsculo.
+    """
+    if not isinstance(value, str):
+        return value
+
+    # Lista de siglas que devem permanecer em maiúsculo.
+    # Você pode adicionar ou remover siglas aqui conforme sua necessidade.
+    ACRONYMS = ['FGTS', 'DAS', 'NR', 'GPS', 'IRRF', 'PIS', 'COFINS', 'CSLL', 'MEI']
+
+    words = value.split()
+    processed_words = []
+    for word in words:
+        # Remove pontuações comuns para uma verificação mais limpa
+        clean_word = word.strip('.,:;')
+        if clean_word.upper() in ACRONYMS:
+            # Se for uma sigla, mantenha em maiúsculo
+            processed_words.append(word.upper())
+        else:
+            # Caso contrário, aplique a capitalização padrão
+            processed_words.append(word.title())
+            
+    return ' '.join(processed_words)
